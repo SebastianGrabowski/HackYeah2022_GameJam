62093401b20dab468210b020d61044f893b65c3a
Łukasz S <lukaszsurma73@gmail.com>
2022-11-20 00:07:34 +0100

    changes

---
+++ HackYeah2022_GameJam/Assets/Scripts/UI/ResourceAmountPopup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResourceAmountPopup : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _Amount;
+    
+    public void SetAmount(int amount)
+    {
+        if(amount > 0) _Amount.text = "+"+amount;
+        else _Amount.text = "-"+amount;
+
+        _Amount.gameObject.SetActive(true);
+    }
+}
