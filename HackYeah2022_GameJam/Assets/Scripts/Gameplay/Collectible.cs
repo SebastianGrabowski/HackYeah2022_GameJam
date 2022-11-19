118e6287115af5862dfb76cfa6189068d78ba0b6
Łukasz S <lukaszsurma73@gmail.com>
2022-11-19 21:44:15 +0100

    sheep collection changes

--- HackYeah2022_GameJam/Assets/Scripts/Gameplay/Collectible.cs
+++ HackYeah2022_GameJam/Assets/Scripts/Gameplay/Collectible.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using Game.Data;
 using UnityEngine;
 
+public enum CollectibleType
+{
+    Wood,
+    Wool,
+    Wheat
+}
+
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private int _Amount;
@@ -10,8 +17,14 @@ public class Collectible : MonoBehaviour
     [SerializeField] private Vector2 _CollectionProgressOffset;
 
     [SerializeField] private Animator _Animator;
+    [SerializeField] private Sprite _SpriteCollected;
+    [SerializeField] private SpriteRenderer _SpriteRenderer;
     [SerializeField] private ResourceData _ResourceData;
     [SerializeField] private ProgressBar _ProgressBar;
+
+    [SerializeField] private CollectibleType _CollectibleType;
+
+    private Sprite _StartSprite;
 
     private bool _CanBeCollected;
     private bool _IsCollected; 
@@ -19,6 +32,12 @@ public class Collectible : MonoBehaviour
 
     private float _TimeToElapse = 0;
     
+
+    void Awake()
+    {
+        _StartSprite = _SpriteRenderer.sprite;
+    }
+
     private void OnMouseOver()
     {
         _IsOver = true;
@@ -37,11 +56,28 @@ public class Collectible : MonoBehaviour
 
     void Update()
     {
-        if(_IsCollected && Time.time >= _TimeToElapse)
+        if(_CollectibleType == CollectibleType.Wood)
         {
-            _Animator.SetTrigger("Collect");
-            Invoke(nameof(Collected), 1f);
+            if(_IsCollected && Time.time >= _TimeToElapse)
+            {
+                _Animator.SetTrigger("Collect");
+                Invoke(nameof(Collected), 1f);
+            }
         }
+        else if(_CollectibleType == CollectibleType.Wool)
+        {
+            if(_IsCollected && Time.time >= _TimeToElapse)
+            {
+                RespawnWool();
+            }
+        }
+           
+    }
+
+    private void RespawnWool()
+    {
+        _SpriteRenderer.sprite = _StartSprite;
+        _IsCollected = false;
     }
 
     public void Collect()
@@ -56,6 +92,12 @@ public class Collectible : MonoBehaviour
         progressBar.SetProgressValue(_CollectionTime);
 
         Destroy(progressBar.gameObject, (_CollectionTime - 0.05f));
+
+        if(_CollectibleType == CollectibleType.Wool) 
+        {
+            _SpriteRenderer.sprite = _SpriteCollected;
+            Game.Gameplay.GameplayController.Instance.Resources[_ResourceData.ID] += _Amount;
+        }
 
         _IsCollected = true;
     }
